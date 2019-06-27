b92b635201564f705eb1820292a326d45c854ce5
Saksham <gupta.10@iitj.ac.in>
2019-06-27 18:15:43 +0530

    Update ecosystem.config.js

--- back-end/ecosystem.config.js
+++ back-end/ecosystem.config.js
@@ -1,45 +1,32 @@
 module.exports = {
-    apps : [{
-      name: 'prsReporter',
-      script: 'index.js',
-  
-      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      args: 'one two',
-      instances: 1,
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: "CB",
+      script: "./back-end/index.js",
       cwd: process.cwd(),
       env: {
-          COMMON_VARIABLE: 'true'
-        },
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
+        COMMON_VARIABLE: "true"
+      },
       env_production: {
-          "PORT": process.env.FREYA_PRODUCTION_PORT,
-          "NODE_ENV": "production",
-          "HOST": process.env.MYSQL_URL,
-          "USER": process.env.MYSQL_USERNAME,
-          "PASSWORD": process.env.MYSQL_PASSWORD,
-          "DATABASE": process.env.MYSQL_DATABASE,
-        },
-        env_staging: {
-          "PORT": process.env.FREYA_PRODUCTION_PORT,
-          "NODE_ENV": "production",
-          "HOST": process.env.MYSQL_URL,
-          "USER": process.env.MYSQL_USERNAME,
-          "PASSWORD": process.env.MYSQL_PASSWORD,
-          "DATABASE": process.env.MYSQL_DATABASE,
-        }
-    }],
-  
-    deploy : {
-      production : {
-        user : 'node',
-        host : '212.83.163.1',
-        ref  : 'origin/master',
-        repo : 'git@github.com:repo.git',
-        path : '/var/www/production',
-        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+        PORT: process.env.CB_PRODUCTION_PORT,
+        NODE_ENV: "production",
+        HOST: process.env.CB_MYSQL_URL_HOST,
+        USER: process.env.CB_MYSQL_USERNAME_USER,
+        PASSWORD: process.env.CB_MYSQL_PASSWORD,
+        DATABASE: process.env.CB_MYSQL_DATABASE
+      },
+      env_staging: {
+        PORT: process.env.CB_PRODUCTION_PORT,
+        NODE_ENV: "production",
+        HOST: process.env.CB_MYSQL_URL_HOST,
+        USER: process.env.CB_MYSQL_USERNAME_USER,
+        PASSWORD: process.env.CB_MYSQL_PASSWORD,
+        DATABASE: process.env.CB_MYSQL_DATABASE
       }
     }
-  };
-  
+  ]
+};
